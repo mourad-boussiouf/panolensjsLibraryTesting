93a022daabc7517bff71bfe2ee6dea039762845e
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-12-26 21:56:34 +0100

    test pano via changement texture babylon js

--- js/visitebabylon.js
+++ js/visitebabylon.js
@@ -1,2 +1,10 @@
 var canvas = document.getElementById("renderCanvas");
 var engine = new BABYLON.Engine(canvas, true);
+
+var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 100}, scene);
+
+var panoramaMaterial = new BABYLON.Panorama360Material("panorama", scene);
+
+panoramaMaterial.panoramaTexture = new BABYLON.Texture("path/to/panorama.jpg", scene);
+
+sphere.material = panoramaMaterial;
