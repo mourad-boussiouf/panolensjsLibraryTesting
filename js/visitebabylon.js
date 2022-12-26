e8618d3964a94bfb23e8532ff335272f7fff5544
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-12-26 21:57:50 +0100

    test function change panorama chronologique par inedx

--- js/visitebabylon.js
+++ js/visitebabylon.js
@@ -8,3 +8,28 @@ var panoramaMaterial = new BABYLON.Panorama360Material("panorama", scene);
 panoramaMaterial.panoramaTexture = new BABYLON.Texture("path/to/panorama.jpg", scene);
 
 sphere.material = panoramaMaterial;
+
+var panoramaTextures = [
+    "path/to/panorama1.jpg",
+    "path/to/panorama2.jpg",
+    "path/to/panorama3.jpg"
+  ];
+  
+  var currentPanoramaIndex = 0;
+  
+  function changePanorama() {
+
+    panoramaMaterial.panoramaTexture = new BABYLON.Texture(panoramaTextures[currentPanoramaIndex], scene);
+  
+    currentPanoramaIndex++;
+
+    if (currentPanoramaIndex >= panoramaTextures.length) {
+      currentPanoramaIndex = 0;
+    }
+  }
+  
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 38) { 
+      changePanorama();
+    }
+  });
