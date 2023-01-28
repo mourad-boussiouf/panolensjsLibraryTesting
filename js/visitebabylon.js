27812927c95f54190de249c70822ead36b262f16
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-01-28 21:51:24 +0100

    nouvelle structure de donnée test pour visite davantage auto générées

--- js/visitebabylon.js
+++ js/visitebabylon.js
@@ -9,15 +9,38 @@ panoramaMaterial.panoramaTexture = new BABYLON.Texture("path/to/panorama.jpg", s
 
 sphere.material = panoramaMaterial;
 
-var panoramaTextures = [
-    "path/to/panorama1.jpg",
-    "path/to/panorama2.jpg",
-    "path/to/panorama3.jpg"
-  ];
+var horloge {4
+  {1 => }
+}
+
+actualOrientation = BabylonYawBidule();
+actualPano = panoramasTexturesNodes.a[1];
+NextPano = determinateNextPano(actualOrientation, actualPano)
+
+var panoramasTexturesNodes = {
+a: { 
+    1: "path/to/panorama1.jpg", 2: "path/to/panorama2.jpg", 3: "path/to/panorama3.jpg",
+    4: "path/to/panorama4.jpg", 5: "path/to/panorama5.jpg", 6: "path/to/panorama6.jpg",
+    7: "path/to/panorama7.jpg", 8: "path/to/panorama8.jpg", 9: "path/to/panorama9.jpg",
+    10: "path/to/panorama10.jpg", 11: "path/to/panorama11.jpg", 12: "path/to/panorama12.jpg",
+    },
+b: { 
+    1: "path/to/panorama1.jpg", 2: "path/to/panorama2.jpg", 3: "path/to/panorama3.jpg",
+    4: "path/to/panorama4.jpg", 5: "path/to/panorama5.jpg", 6: "path/to/panorama6.jpg",
+    7: "path/to/panorama7.jpg", 8: "path/to/panorama8.jpg", 9: "path/to/panorama9.jpg",
+    10: "path/to/panorama10.jpg", 11: "path/to/panorama11.jpg", 12: "path/to/panorama12.jpg",
+    },
+c: { 
+    1: "path/to/panorama1.jpg", 2: "path/to/panorama2.jpg", 3: "path/to/panorama3.jpg",
+    4: "path/to/panorama4.jpg", 5: "path/to/panorama5.jpg", 6: "path/to/panorama6.jpg",
+    7: "path/to/panorama7.jpg", 8: "path/to/panorama8.jpg", 9: "path/to/panorama9.jpg",
+    10: "path/to/panorama10.jpg", 11: "path/to/panorama11.jpg", 12: "path/to/panorama12.jpg",
+  },
+};
   
   var currentPanoramaIndex = 0;
   
-  function changePanorama() {
+  function determinateNextPano() {
 
     panoramaMaterial.panoramaTexture = new BABYLON.Texture(panoramaTextures[currentPanoramaIndex], scene);
   
@@ -29,7 +52,7 @@ var panoramaTextures = [
   }
   
   window.addEventListener("keydown", function(event) {
-    if (event.key === "arrowDown") { 
-      changePanorama();
+    if (event.key === "arrowUp") { 
+      determinateNextPano();
     }
   });
