0069f5418adc173820b0e5a9572db5f826b43850
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-01-29 22:19:40 +0100

    ajout actualPanoArray avec toutes orientations + le actuelPano avec texture unique actuelle

--- js/visitebabylon.js
+++ js/visitebabylon.js
@@ -9,28 +9,26 @@ panoramaMaterial.panoramaTexture = new BABYLON.Texture("path/to/panorama.jpg", s
 
 sphere.material = panoramaMaterial;
 
-var horloge {4
-  {1 => }
-}
 
 actualOrientation = BabylonYawBidule();
-actualPano = panoramasTexturesNodes.a[1];
+actualPanoArray = panoramasTexturesNodes["1"][1];
+actualPanoArray = panoramasTexturesNodes.a[1];
 NextPano = determinateNextPano(actualOrientation, actualPano)
 
 var panoramasTexturesNodes = {
-a: { 
+"1": { 
     1: "path/to/panorama1.jpg", 2: "path/to/panorama2.jpg", 3: "path/to/panorama3.jpg",
     4: "path/to/panorama4.jpg", 5: "path/to/panorama5.jpg", 6: "path/to/panorama6.jpg",
     7: "path/to/panorama7.jpg", 8: "path/to/panorama8.jpg", 9: "path/to/panorama9.jpg",
     10: "path/to/panorama10.jpg", 11: "path/to/panorama11.jpg", 12: "path/to/panorama12.jpg",
     },
-b: { 
+"2": { 
     1: "path/to/panorama1.jpg", 2: "path/to/panorama2.jpg", 3: "path/to/panorama3.jpg",
     4: "path/to/panorama4.jpg", 5: "path/to/panorama5.jpg", 6: "path/to/panorama6.jpg",
     7: "path/to/panorama7.jpg", 8: "path/to/panorama8.jpg", 9: "path/to/panorama9.jpg",
     10: "path/to/panorama10.jpg", 11: "path/to/panorama11.jpg", 12: "path/to/panorama12.jpg",
     },
-c: { 
+"3": { 
     1: "path/to/panorama1.jpg", 2: "path/to/panorama2.jpg", 3: "path/to/panorama3.jpg",
     4: "path/to/panorama4.jpg", 5: "path/to/panorama5.jpg", 6: "path/to/panorama6.jpg",
     7: "path/to/panorama7.jpg", 8: "path/to/panorama8.jpg", 9: "path/to/panorama9.jpg",
@@ -40,7 +38,9 @@ c: {
   
   var currentPanoramaIndex = 0;
   
-  function determinateNextPano() {
+  function determinateNextPano(actualOrientation, actualPano) {
+
+    if()
 
     panoramaMaterial.panoramaTexture = new BABYLON.Texture(panoramaTextures[currentPanoramaIndex], scene);
   
